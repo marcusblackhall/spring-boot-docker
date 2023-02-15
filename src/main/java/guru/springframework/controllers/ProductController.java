794a98ff29f18385633e986e8318a0aecd6a0fe3
Marcus Blackhall <marcusinamsterdam@gmail.com>
2023-02-15 19:21:30 +0100

    Docker config using 4 images

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -1,23 +1,38 @@
 package guru.springframework.controllers;
 
+import guru.springframework.model.events.PageViewEvent;
+import guru.springframework.pageview.PageViewService;
 import guru.springframework.services.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * Created by jt on 1/20/16.
  */
 @Controller
+
 public class ProductController {
 
+    Logger logger = LoggerFactory.getLogger(ProductController.class);
     private ProductService productService;
 
+    private PageViewService pageViewService;
+
     @Autowired
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, PageViewService pageViewService
+
+                             ) {
         this.productService = productService;
+        this.pageViewService = pageViewService;
+
     }
 
     @RequestMapping("/product/{id}")
@@ -25,6 +40,13 @@ public class ProductController {
 
         model.addAttribute("product", productService.getProduct(id));
 
+        PageViewEvent pageViewEvent = new PageViewEvent();
+        pageViewEvent.setPageUrl("springframework.guru/product/" + id);
+        pageViewEvent.setPageViewDate(new Date());
+        pageViewEvent.setCorrelationId(UUID.randomUUID().toString());
+
+  logger.debug("callling page service");
+        pageViewService.sendPageViewEvent(pageViewEvent);
         return "product";
     }
 }
